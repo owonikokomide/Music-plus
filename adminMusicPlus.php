a897517c9a791c495566a92bb8b1067dea0a324a
owonikoko Ayomide <donaykoko@yahoo.com>
2024-06-21 22:14:51 +0100

    Update adminMusicPlus.php

--- adminMusicPlus.php
+++ adminMusicPlus.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/solid.css">
   <link rel="stylesheet" href="fontawesome-free-6.2.1-web/css/brands.css">
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">  
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin MusicPlus</title>
 </head>
